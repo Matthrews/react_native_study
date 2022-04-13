80e44c539d25305882527ba25c854b3a90b44976
Oliver <zhurongfu@opsmind.com>
2022-04-14 00:24:55 +0800

    4. TextInput

--- day01/App.js
+++ day01/App.js
@@ -1,19 +1,39 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, TextInput } from "react-native";
 // Views, Text & Styles
 
 export default function App() {
   const [name, setName] = useState("Jason");
+  const [age, setAge] = useState("25");
+  const [address, setAddress] = useState("Shanghai");
 
   const changeName = (event) => {
     console.log("changeName", event);
-    setName("matthew")
-  }
+    setName("matthew");
+  };
 
   return (
     <View style={styles.container}>
-      <Text>My name is {name} </Text>
+      <Text>Enter your address: </Text>
+      <TextInput
+        style={styles.inputWrapper}
+        value={address}
+        autoFocus
+        placeholder="e.g. Florida"
+        onChangeText={(text) => setAddress(text)}
+      />
+      <Text>Enter your age: </Text>
+      <TextInput
+        style={styles.inputWrapper}
+        value={age}
+        keyboardType="numeric"
+        placeholder="e.g. 24"
+        onChangeText={(text) => setAge(text)}
+      />
+      <Text>
+        My name is {name}, I am { age } years old, and I live in { address }
+      </Text>
       <View style={styles.btnContainer}>
         <Button title="update name" onPress={changeName} />
       </View>
@@ -30,5 +50,13 @@ const styles = StyleSheet.create({
   },
   btnContainer: {
     marginTop: 32,
-  }
+  },
+  inputWrapper: {
+    borderWidth: 1,
+    borderColor: "#777",
+    borderRadius: 4,
+    padding: 8,
+    margin: 10,
+    width: 200,
+  },
 });
